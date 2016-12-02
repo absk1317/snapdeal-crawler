04468092fb0c058fd19e90961cf8cfc18acc726a
abhishek verma <abhishek.verma@cybrilla.com>
2016-12-02 15:15:50 +0530

    adds action methods in crawl controller

---
+++ app/controllers/crawls_controller.rb
@@ -0,0 +1,33 @@
+class CrawlsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @crawl = current_user.crawls.new
+  end
+
+  def create
+    Crawl.create(crawl_params)
+    redirect_to root_path
+  end
+
+  def index
+    @crawls = current_user.crawls
+  end
+
+  def show
+    @crawl = Crawl.find(params[:id])
+    @products = @crawl.products
+  end
+
+  def destroy
+    @crawl = Crawl.find(params[:id])
+    @crawl.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def crawl_params
+    params.require(:crawl).permit(:url).merge(user_id: current_user.id)
+  end
+end
