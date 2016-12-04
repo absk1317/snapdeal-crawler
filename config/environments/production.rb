06a1055608eb10e13c305332ae1ae8b52bff05b6
abhishek verma <abhishek.verma@cybrilla.com>
2016-12-05 00:24:09 +0530

    add letter opener for heroku environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
   config.action_mailer.default_url_options = {
-    host: 'localhost:3000', protocol: 'http'
+    host: 'snapdeal.herokuapp.com', protocol: 'http'
   }
   config.action_mailer.delivery_method = :letter_opener_web
   # Send deprecation notices to registered listeners.
